e7cc5ac1e3684002e9f2c3800bd6c7d2df82d0c1
jgh <jon.heiser@gmail.com>
2022-01-08 15:48:38 +0000

    init

--- Pet.gd
+++ Pet.gd
@@ -2,9 +2,12 @@ extends Node2D
 
 export (float) var min_fill_eye_scale_threshold_pct = 25
 export (float) var eye_top_offset_max = 17
+export (float) var mouth_top_offset_max = 36
 
 onready var eyes_animation_player = $FillLevel/EyesSprite/AnimationPlayer
 onready var blink_timer = $FillLevel/EyesSprite/BlinkTimer
+onready var mouth = $FillLevel/Mouth
+onready var eyes = $FillLevel/EyesSprite
 
 onready var fill_max_pos = $FillMax
 onready var fill_min_pos = $FillMin
@@ -27,12 +30,25 @@ func set_fill_level_pct(pct: float) -> void:
 
 	if pct < min_fill_eye_scale_threshold_pct:
 		var scale =  pct / min_fill_eye_scale_threshold_pct
-		$FillLevel/EyesSprite.scale.y = scale
-		#$FillLevel/EyesSprite.position.y = eyes_offset * scale
+		eyes.scale.y = scale
+		mouth.scale.y = scale
 	else:
-		$FillLevel/EyesSprite.scale.y = 1.0
-	$FillLevel/EyesSprite.position.y = eye_top_offset_max * (pct/100.0)
+		eyes.scale.y = 1.0
+		mouth.scale.y = 1.0
+	eyes.position.y = eye_top_offset_max * (pct/100.0)
+	mouth.position.y = mouth_top_offset_max * (pct/100.0)
+
+	var mouth_mode = 0
+	if 35 > pct and pct > 20:
+		mouth_mode = 1
+	elif pct <= 20:
+		mouth_mode = 2
+
+	mouth.texture.region = Rect2(
+		Vector2(mouth_mode * 64, 0), 
+		mouth.texture.region.size)
 	
+		
 
 
 
