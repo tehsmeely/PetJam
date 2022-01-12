88eed60f7a99a47685b1fcf872fc8388be13d488
Jonty <jon.heiser@gmail.com>
2022-01-12 22:09:19 +0000

    New: Buy Shelf and Flour

---
+++ BuyShelf.gd
@@ -0,0 +1,21 @@
+extends Node2D
+
+export (String) var item_name
+export (String, MULTILINE) var item_tooltip
+export (int) var item_cost
+
+onready var label = $Label
+onready var tooltip_question_mark = $Panel
+onready var flour = $Flour
+onready var buy_button = $Button
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	label.text = "%s: %d" % [item_name, item_cost]
+	tooltip_question_mark.hint_tooltip = item_tooltip
+	var _err = buy_button.connect("pressed", self, "_on_buy_button_pressed")
+
+
+func _on_buy_button_pressed() -> void:
+	flour.buy()
